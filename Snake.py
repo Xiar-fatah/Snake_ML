128bdb5545f7cd6c35240387fd41013b7e621ce6
Xiar-fatah <xiar.fatah@gmail.com>
2019-11-24 22:32:41 +0100

    Update Snake.py

--- Snake.py
+++ Snake.py
@@ -10,7 +10,7 @@ pygame.display.set_caption("Snake")
 Bool = True
 
 
-class player(object):
+class Snake(object):
     
     def __init__(self,x,y,width,height):
         self.x = x
@@ -19,18 +19,19 @@ class player(object):
         self.height = height
         self.vel = 5
         self.hitbox = (self.x, self.y, self.width, self.height)
-
+        self.rect = pygame.Rect(self.hitbox)
 
 
 
     def draw(self, screen):
         
-        
+        self.rect = pygame.Rect(self.hitbox) #Values gets updated heres
+
         pygame.draw.rect(screen, (254,255,242), (self.x, self.y, self.width, self.height))
         self.hitbox = (self.x, self.y, self.height,  self.width) #Updating the position of the hitbox
         pygame.draw.rect(screen, (255,0,0), self.hitbox, 2)
         
-class apple(object):
+class Apple(object):
     
     def __init__(self,x,y,width,height):
         self.x = x
@@ -38,20 +39,24 @@ class apple(object):
         self.width = width
         self.height = height
         self.hitbox = (self.x, self.y, self.width, self.height) #initial position of the hitbox
+        self.test = (self.x, self.y, self.width, self.height)
 
-
+        self.rect = pygame.Rect(self.test)
 
     def draw(self, screen):
 
         pygame.draw.rect(screen, (100,255,242), (self.x, self.y, self.width, self.height))
         self.hitbox = (self.x, self.y, self.height,  self.width) #Updating the position of the hitbox
         pygame.draw.rect(screen, (255,0,0), self.hitbox, 2)
+        
+    def is_collided_with(self, snake):
+        return self.rect.colliderect(snake.rect)
 
 
     
 def redrawGameWindow():
-    man.draw(screen)
-    points.draw(screen)
+    snake.draw(screen)
+    apple.draw(screen)
 
     
     pygame.display.update()
@@ -60,9 +65,10 @@ def redrawGameWindow():
 
 
 
+Bool = True
 
-man = player(50, 50, 15, 15)
-points = apple(100, 100, 15, 15)
+snake = Snake(50, 50, 15, 15)
+apple = Apple(100, 100, 15, 15)
 vel = 5
 while Bool:
     pygame.time.delay(10) # This will delay the game the given amount of milliseconds. In our casee 0.1 seconds will be the delay
@@ -71,28 +77,30 @@ while Bool:
         if event.type == pygame.QUIT: # Checks if the red button in the corner of the window is clicked
             Bool = False
 
+    if apple.is_collided_with(snake):
+        print('collision!')
+        ##generate a new apple with random position
+    
     keys = pygame.key.get_pressed()
     
     if keys[pygame.K_LEFT]:
-        man.x -= vel
+        snake.x -= vel
 
     if keys[pygame.K_RIGHT]:
-        man.x += vel
+        snake.x += vel
 
     if keys[pygame.K_UP]:
-        man.y -= vel
+        snake.y -= vel
 
     if keys[pygame.K_DOWN]:
-        man.y += vel
+        snake.y += vel
 
     screen.fill((0,0,0))
     
-
     redrawGameWindow()
 
 
 pygame.quit() 
-
 
 
 
